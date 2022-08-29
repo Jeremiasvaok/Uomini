e0588ee5f4d2b220a93fce1715fde687d6ad34b4
jeremias <jeremias.vallejos@gmail.com>
2022-08-29 19:55:38 -0300

    mmm

--- Back-end/src/controllers/products/index.js
+++ Back-end/src/controllers/products/index.js
@@ -5,22 +5,32 @@ module.exports ={
         const find = await Products.find()
         res.send(find)
     },
+
     postProducts:async (req,res)=>{
         const {name, description, price} = req.body
+    //   if(!name){
+    //     return res.status(404).send('Falta informacion el NOMBRE es incorrecto')
+    //   }
+    //   if(!description){
+    //     return res.send('Falta informacion la DESCRIPCION es incorrecta')
+    //   }
+    //   if(!price){
+    //     return res.send('Falta informacion el PRECIO es incorrecto')
+    //   }
+    //   else{
         const product = new Products({
             name,
             description,
             price
         })
         product.save()
-                .then(()=>{
-                    return res.send(product)
-                })
-                .catch((error)=>{
-                    console.log(error)
-                })
-    }
-
+        .then(()=>{
+            return res.status(201).json({msg:'Producto creado', product})
+            })
+        .catch((error)=>{
+             console.log(error)
+        })
+}
 
 
 
--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const router = require('./routers/index')
 require('./mongodb')
 
@@ -9,7 +10,10 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors())
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost3000"
+}))
 app.use('/',router)
 
 module.exports = app
