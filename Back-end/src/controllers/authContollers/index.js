926b67df1e0ca8a0fd2b0f0ef6f93062acfd4aa2
jeremias <jeremias.vallejos@gmail.com>
2022-09-06 16:02:04 -0300

    kk

--- Back-end/src/controllers/authContollers/index.js
+++ Back-end/src/controllers/authContollers/index.js
@@ -11,18 +11,19 @@ module.exports ={
             username,
             email,
             password: await User.encryptPassword(password),
-            
+            roles,
+        
           })
           if(roles){
             const foundRole = await Role.find({name:{$in:roles}})
-            newUser.roles = foundRole.map(role => role._id)
+            newUser.roles = foundRole.map((role) => role._id)
           }else{
-            const role = await Role.findOne({name:"user"})
+            const role = await Role.find({name:"user"})
             newUser.roles = [role._id]
           }
 
          const saveUser = await newUser.save()
-        const token =  jwt.sign({id: saveUser._id}, 'PRODUCTS',{ expiresIn: 86400 /*24 horas */}//crea un token
+        const token =  jwt.sign({id: saveUser._id}, 'PRODUCTS',{ expiresIn: 86400 /*expira 24 horas */}//crea un token
           ) 
           res.json({token})
      } catch (error) {
