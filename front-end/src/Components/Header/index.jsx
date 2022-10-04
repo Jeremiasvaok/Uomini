7f106b56fa7cd8bf9c81433413e980472b69440a
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-10-04 16:44:58 -0300

    arreglos

---
+++ front-end/src/Components/Header/index.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>Remeras</ul>
+                    <ul>Pantalones</ul>
+                    <ul>Abrigos</ul>
+                    <ul>Camisas</ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header
