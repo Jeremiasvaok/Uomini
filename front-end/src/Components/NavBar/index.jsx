4cc92d10d6f9b12acc8db5425a10482195d7b0c0
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-10-28 15:49:18 -0300

    arrg

--- front-end/src/Components/NavBar/index.jsx
+++ front-end/src/Components/NavBar/index.jsx
@@ -1,35 +1,21 @@
-import { AppBar, Toolbar, makeStyles} from "@material-ui/core"
+
 import { Link } from "react-router-dom"
 
-const useStyles = makeStyles(theme => ({
-    offset: theme.mixins.toolbar,
-    ul:{
-        listStyleType: 'none',
-    },
-    lista:{
-        color: 'black',
-        listStyle: 'none',
-        float: 'left',
-        textAlign: 'center',
-        width: '5em',
-    }
-}))
-
 const NavBar = () => {
-    const classes = useStyles()
+   
     return (                                                          
         <div>
-            <AppBar>
-                <Toolbar>
-                    <ul className={classes.ul} >
-                    <Link to='/remeras'><li className={classes.lista}>Remeras</li></Link>
-                    <Link to='/pantalones'><li className={classes.lista}>Pantalones</li></Link>
-                    <Link to='/abrigos'><li className={classes.lista}>Abrigos</li></Link>
-                    <Link to='/camisas'><li className={classes.lista}>Camisas</li></Link>
-                    <Link to='/carro'><li className={classes.lista}>Carro</li></Link>    
+            <header>
+                <div>
+                    <ul>
+                    <Link to='/remeras'><li>Remeras</li></Link>
+                    <Link to='/pantalones'><li>Pantalones</li></Link>
+                    <Link to='/abrigos'><li>Abrigos</li></Link>
+                    <Link to='/camisas'><li>Camisas</li></Link>
+                    <Link to='/carro'><li>Carro</li></Link>    
                     </ul>
-                </Toolbar>
-            </AppBar>
+                </div>
+            </header>
             <div className={classes.offset}></div>
         </div>
     )
