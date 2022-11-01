5ed7a3e22b8346dab9190a779abf08628a7bd268
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-11-01 16:30:25 -0300

    css

--- front-end/src/Components/NavBar/index.jsx
+++ front-end/src/Components/NavBar/index.jsx
@@ -2,21 +2,20 @@ import { Link } from "react-router-dom"
 import styles from './navbar.module.css'
 
 const NavBar = () => {
-   
-    return (                                                          
-        <div>
-            <header>
+
+    return (
+        <div clasName='headers'>
+            <nav>
                 <div>
                     <ul className={styles.conteiner}>
-                    <Link to='/remeras'><li>Remeras</li></Link>
-                    <Link to='/pantalones'><li>Pantalones</li></Link>
-                    <Link to='/abrigos'><li>Abrigos</li></Link>
-                    <Link to='/camisas'><li>Camisas</li></Link>
-                    <Link to='/carro'><li>Carro</li></Link>    
+                        <Link to='/remeras'><li>Remeras</li></Link>
+                        <Link to='/pantalones'><li>Pantalones</li></Link>
+                        <Link to='/abrigos'><li>Abrigos</li></Link>
+                        <Link to='/camisas'><li>Camisas</li></Link>
+                        <Link to='/carro'><li>Carro</li></Link>
                     </ul>
                 </div>
-            </header>
-            <div></div>
+            </nav>
         </div>
     )
 }
