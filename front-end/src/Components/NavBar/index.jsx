37eec23350e9391cdd79c26a902a6725f1b83759
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-11-03 19:48:20 -0300

    css

--- front-end/src/Components/NavBar/index.jsx
+++ front-end/src/Components/NavBar/index.jsx
@@ -12,41 +12,41 @@ const NavBar = () => {
                   <ul className={styles.conteiner}>
                     <li><Link to='/remeras'>Remeras</Link>
                     <ul>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
+                        <li><a href='#'>SubMenu</a></li>
+                        <li><a href='#'>SubMenu</a></li>
+                        <li><a href='#'>SubMenu</a></li>
                     </ul>
                     </li>
 
                     <li><Link to='/pantalones'>Pantalones</Link>
                     <ul>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
                     </ul>
                     </li>
 
                     <li><Link to='/abrigos'>Abrigos</Link>
                     <ul>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
                     </ul>
                     </li>
 
                     <li><Link to='/camisas'>Camisas</Link>
                     <ul>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
                     </ul>
                     </li>
 
                     <li><Link to='/carro'>Carro</Link>
                     <ul>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
-                        <li>SubMenu</li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
+                    <li><a href='#'>SubMenu</a></li>
                     </ul>
                     </li>
                 </ul>
