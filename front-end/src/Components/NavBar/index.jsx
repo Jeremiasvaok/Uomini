841c584d9386c504cbcb47b4a21ec340e00447f7
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-11-04 17:41:24 -0300

    css

--- front-end/src/Components/NavBar/index.jsx
+++ front-end/src/Components/NavBar/index.jsx
@@ -1,61 +1,62 @@
 import { Link } from "react-router-dom"
 import styles from './navbar.module.css'
-import home from "../../img/imagen.jpg"
+import lupa from "../../img/lupa.png"
 
 const NavBar = () => {
 
     return (
         <>
-        <header>
-            <div className={styles.contenedor_menu}>
-                <div className={styles.menu}>
-                    <nav>
-                  <ul className={styles.conteiner}>
-                    <li><Link to='/remeras'>Remeras</Link>
-                    <ul>
-                        <li><a href='#'>SubMenu</a></li>
-                        <li><a href='#'>SubMenu</a></li>
-                        <li><a href='#'>SubMenu</a></li>
-                    </ul>
-                    </li>
+            <header>
+                <div className={styles.contenedor_menu}>
+                    <div className={styles.menu}>
+                        <nav>
+                            <ul className={styles.conteiner}>
+                                <li><Link to='/remeras'>Remeras</Link>
+                                    <ul>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                    </ul>
+                                </li>
 
-                    <li><Link to='/pantalones'>Pantalones</Link>
-                    <ul>
-                    <li><a href='#'>SubMenu</a></li>
-                    <li><a href='#'>SubMenu</a></li>
-                    <li><a href='#'>SubMenu</a></li>
-                    </ul>
-                    </li>
+                                <li><Link to='/pantalones'>Pantalones</Link>
+                                    <ul>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                    </ul>
+                                </li>
 
-                    <li><Link to='/abrigos'>Abrigos</Link>
-                    <ul>
-                    <li><a href='#'>SubMenu</a></li>
-                    <li><a href='#'>SubMenu</a></li>
-                    <li><a href='#'>SubMenu</a></li>
-                    </ul>
-                    </li>
+                                <li><Link to='/abrigos'>Abrigos</Link>
+                                    <ul>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                    </ul>
+                                </li>
 
-                    <li><Link to='/camisas'>Camisas</Link>
-                    <ul>
-                    <li><a href='#'>SubMenu</a></li>
-                    <li><a href='#'>SubMenu</a></li>
-                    <li><a href='#'>SubMenu</a></li>
-                    </ul>
-                    </li>
+                                <li><Link to='/camisas'>Camisas</Link>
+                                    <ul>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                        <li><a href='#'>SubMenu</a></li>
+                                    </ul>
+                                </li>
 
-                    {/* <li><Link to='/carro'>Carro</Link>
+                                {/* <li><Link to='/carro'>Carro</Link>
                     <ul>
                     <li><a href='#'>SubMenu</a></li>
                     <li><a href='#'>SubMenu</a></li>
                     <li><a href='#'>SubMenu</a></li>
                     </ul>
                     </li> */}
-                    {/* <span>{home}</span> */}
-                    <img src={home} />
-                </ul>
-                </nav>
+                                {/* <span>{home}</span> */}
+                               <input type={'search'} placeholder='Busque un producto'/>
+                                <button>{lupa}</button>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-            </div>
             </header>
         </>
     )
