330bb8b1431c3c285733dec29f5d7c6722c42a73
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-11-24 15:57:58 -0300

    css

--- front-end/src/Components/Home/index.jsx
+++ front-end/src/Components/Home/index.jsx
@@ -1,6 +1,8 @@
 import NavBar from '../NavBar'
 import { Link} from 'react-router-dom'
-import imagenNeon from '../../img/pantalon.png'
+import imagenNeonRemera from '../../img/imagenneonpantalon.png'
+import imagenNeonPantalon from '../../img/pantalon.png'
+
 import styles from './home.module.css'
 
 const Landing = ()=>{
@@ -10,9 +12,12 @@ const Landing = ()=>{
             <NavBar/>
         </div>
         <div>
-            <Link to='/pantalones'><img className={styles.imagePant} src={imagenNeon} alt='pantalon'/></Link>
+            <Link to='/pantalones'><img className={styles.imagePant} src={imagenNeonPantalon} alt='pantalon'/></Link>
         </div>
-
+        <div>
+            <Link to='/remeras'><img className={styles.imagePant} src={imagenNeonRemera} alt='remeras'/></Link>
+        </div>
+        
         </>
     )
 }
