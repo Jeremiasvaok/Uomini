a742f90ad0acefb807ed6d66165fc071fb8d66b0
jeremias vallejos <jeremias.vallejos78@gmail.com>
2022-11-24 14:19:46 -0300

    home

--- front-end/src/Components/Home/index.jsx
+++ front-end/src/Components/Home/index.jsx
@@ -2,12 +2,20 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector} from 'react-redux'
 import NavBar from '../NavBar'
 import { getProducts } from '../../Redux/Actions'
+import imagenNeon from '../../img/pantalon.png'
+import styles from './home.module.css'
 
 const Landing = ()=>{
     return(
+        <>
         <div>
             <NavBar/>
         </div>
+        <div>
+            <img className={styles.imagePant} src={imagenNeon} alt='pantalon'/>
+        </div>
+
+        </>
     )
 }
 
